983a0ac79dce080be1b54d9c6e6ac8c6c02228ba
Dervine <ndervine@sendy.co.ke>
2018-06-20 16:47:01 +0300

    set shipping

--- custom/setShipping.php
+++ custom/setShipping.php
@@ -19,4 +19,6 @@ $default_country = null;
 $product_list = null;
 $id_zone = null;
 
+$sendyapicart->setShippingCost($shipping_cost);
+
 echo $sendyapicart->getPackageShippingCost($id_carrier, $use_tax,$default_country, $product_list, $id_zone, $shipping_cost);

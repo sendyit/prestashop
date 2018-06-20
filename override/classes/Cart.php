097eef9cd3950e99061ae9d2b1242ddeac5f8913
Dervine <ndervine@sendy.co.ke>
2018-06-20 10:35:26 +0300

    set shipping

--- override/classes/Cart.php
+++ override/classes/Cart.php
@@ -6,7 +6,7 @@ class Cart extends CartCore
     * date: 2018-06-19 00:34:52
     * version: 1.0.0
     */
-    public function getPackageShippingCost($id_carrier = null, $use_tax = true, country $default_country = null, $product_list = null, $id_zone = null, $shipping_cost = 100)
+    public function getPackageShippingCost($id_carrier = null, $use_tax = true, country $default_country = null, $product_list = null, $id_zone = null, $shipping_cost = 200)
     {
         return $shipping_cost;
     }
